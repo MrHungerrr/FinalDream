aad3764a782d479f355bb56ef918486a2c944e2c
MrHungerrr <egor06060600@gmail.com>
2019-02-12 20:11:15 +0300

    Super

---
+++ Game/Assets/Scripts/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+
+   public bool power;
+
+
+	void Start () {
+      if (power)
+         this.tag = "actionOn";
+      else
+         this.tag = "actionOff";
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private bool pause = false;
+    
+    public GameObject player;
+    public GameObject PauseUI;
+    public GameObject Pager;
+
+
+    void Update () {
+	
+      if(Input.GetKeyDown(KeyCode.Escape) && !Pager.activeSelf)
+         if(!pause)
+         {
+                Time.timeScale = 0;
+                pause = true;
+                PauseUI.SetActive(true);
+                player.GetComponent<PlayerScript>().enabled = false;
+         }
+         else
+         {
+                Time.timeScale = 1;
+                pause = false;
+                PauseUI.SetActive(false);
+                player.GetComponent<PlayerScript>().enabled = true;
+         }
+
+      if (Input.GetKeyDown(KeyCode.Tab) && !PauseUI.activeSelf)
+      {
+            Pager.SetActive(!Pager.activeSelf);
+            player.GetComponent<PlayerScript>().enabled = !player.GetComponent<PlayerScript>().enabled;
+      }
+
+	}
+}
