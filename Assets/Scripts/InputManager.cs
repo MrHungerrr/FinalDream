c60f88ca664ae4f80013488ed3ca8fadad60d41e
MrHungerrr <egor06060600@gmail.com>
2019-02-12 19:58:37 +0300

    URAAAAAA

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+   [HideInInspector]
+   public bool game = true;
+   private Vector2 inputMove;
+   public PlayerScript player;
+   public ParticleSystem partSosi;
+
+   //private ParticleSystem.VelocityOverLifetimeModule vel;
+   //private ParticleSystem.ShapeModule shape;
+
+
+   void Start()
+   {
+   }
+
+
+   void Update()
+   {
+      if (game)
+         GameInput();
+      else
+         MenuInput();
+   }
+
+   void FixedUpdate()
+   {
+      if (game)
+         GameFixInput();
+   }
+
+
+   private void GameFixInput()
+   {
+      // Передвижение
+      inputMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+      if (inputMove != Vector2.zero)
+      {
+         player.MoveCalculate(inputMove);
+
+         var vel = partSosi.velocityOverLifetime;
+         vel = partSosi.velocityOverLifetime;
+         vel.x = 5 * -inputMove.x;
+         vel.y = 2 * -inputMove.y;
+
+        if (inputMove.y == -1)
+            {
+                partSosi.startLifetime = 4.2f;
+            }
+        else
+            {
+                partSosi.startLifetime = 3.5f;
+            }
+         
+      }
+      else
+      {
+            partSosi.startLifetime = 3f;
+            var vel = partSosi.velocityOverLifetime;
+            vel.x = 0f;
+            vel.y = 0f;
+      }
+
+      //Сила
+      if (Input.GetMouseButtonDown(1))
+      {
+         player.ForceCalculate(true);
+      }
+
+      if (Input.GetMouseButtonUp(1))
+      {
+         player.ForceCalculate(false);
+      }
+   }
+
+
+   private void GameInput()
+   {
+
+      if (Input.GetMouseButtonDown(0))
+      {
+      }
+
+
+      //Смена силы
+      if (Input.GetKeyDown(KeyCode.Q))
+      {
+         player.SwitchForce();
+      }
+
+
+      //Действие или лечение
+      if (Input.GetKey(KeyCode.E) || Input.GetKeyUp(KeyCode.E))
+      {
+         player.RegenHealth();
+      }
+
+
+      //Перезарядка кристалла
+      if (Input.GetKeyDown(KeyCode.R))
+      {
+         player.ReloadForce();
+      }
+
+
+      //Первая способность костюма
+      if (Input.GetKeyDown(KeyCode.LeftShift))
+      {
+         player.Run(true);
+      }
+
+      if (Input.GetKeyUp(KeyCode.LeftShift))
+      {
+         player.Run(false);
+      }
+
+      //Вторая способность костюма
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+      }
+
+
+      //Сохранение
+      if (Input.GetKey(KeyCode.F))
+      {
+         player.Save();
+      }
+
+      //Пейджер
+      if (Input.GetKeyDown(KeyCode.Tab))
+      {
+
+      }
+
+      //Меню
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+
+      }
+   } 
+
+   private void MenuInput()
+   {
+
+   }
+
+}
---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+    public Item[] Items;
+
+    //public Transform  inventoryTransform;
+    public GameObject Pager;
+    public GameObject Container;
+    public Image DescriptionImage;
+    public GameObject file;
+    public GameObject audioNotes;
+    private GameObject notecanavsimage;
+
+    public GameObject PressE;
+    public GameObject NoteChecked;
+
+    public int KristalCount = 0;//количество кристалов
+
+    private string ipath;
+
+    int k = 0;
+    int n = 0;
+
+    private void Start()
+    {
+        Items = new Item[30];
+        notecanavsimage = audioNotes.transform.Find("NoteCanavs").GetChild(0).gameObject;
+    }
+
+    void Update ()
+    {
+        
+        if (Pager.transform.Find("Files").gameObject.activeInHierarchy)
+        {
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) && k < n-1)
+                k++;
+            else if (k > 0 && Input.GetKeyDown(KeyCode.UpArrow))
+                k--;
+
+            if (n>0)
+            {
+                ipath = Items[k].sprite;
+                DescriptionImage.color = new Color(1, 1, 1, 1);
+                DescriptionImage.sprite = Resources.Load<Sprite>(ipath);
+                DescriptionImage.preserveAspect = true;
+                //DescriptionImage.gameObject.AddComponent<Button>().onClick.AddListener(() => OnButtonClick(Items[k]));
+                file.transform.Find("Namefield").GetComponent<Text>().text = Items[k].Name + "\n" + Items[k].timecollected.ToShortDateString() + "\n" + Items[k].timecollected.ToLongTimeString();
+                file.transform.Find("Typefield").GetComponent<Text>().text = Items[k].Type;
+            }
+            else
+                DescriptionImage.color = new Color(0, 0, 0, 0);
+            
+        }
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            /*
+                        if (Pager.activeSelf)
+                        {
+
+                            for (int i = 0; i < Items.Count; i++)
+                            {
+                                Item item = Items[i];
+
+                                if (inventoryTransform.GetChild(i).childCount == 0)
+                                {
+                                    GameObject img = Instantiate(Container, inventoryTransform.GetChild(i));
+                                    img.GetComponent<Image>().sprite = Resources.Load<Sprite>(item.sprite);
+                                    img.GetComponent<Image>().preserveAspect = true;
+                                   
+                                }
+                                else break;
+                            }
+
+                        }
+                        else
+                        {
+                            for (int i = 0; i < inventoryTransform.childCount; i++)
+                            {
+                                if (inventoryTransform.GetChild(i).childCount != 0)
+                                {
+                                    Destroy(inventoryTransform.GetChild(i).GetChild(0).gameObject);
+                                }
+                            }
+                        }
+            */
+        }
+
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Item")
+        {
+            PressE.GetComponent<Text>().text = "Press 'E' to collect " + col.GetComponent<Item>().Name;
+            PressE.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PressE.SetActive(false);
+                Items[n] = col.GetComponent<Item>();
+                Items[n].iscollected = true;
+                Items[n].timecollected = DateTime.Now;
+                n++;
+                if (col.GetComponent<Item>().prefab == ("prefabs/Кристал"))
+                {
+                    KristalCount++;
+                }
+                else if (col.GetComponent<Item>().prefab == "prefabs/Audio")
+                {
+                    audioNotes.GetComponent<AudioSource>().clip = col.GetComponent<AudioSource>().clip;
+                    audioNotes.GetComponent<AudioSource>().Play();
+                }
+                else if (col.GetComponent<Item>().prefab == "prefabs/Note")
+                {
+                    notecanavsimage.GetComponent<Image>().sprite = col.GetComponent<Image>().sprite;
+                    notecanavsimage.GetComponent<Image>().enabled = true;
+                }
+                col.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerExit2D()
+    {
+        PressE.SetActive(false);
+    }
+
+   /* private Transform GetFreeSlot()
+    {
+        foreach (Transform t in inventoryTransform)
+        {
+            if (t.childCount == 0)
+            {
+                return t;
+            }
+        }
+
+        return null;
+    }
+    */
+
+
+    void OnButtonClick(Item obj)
+    {
+        if (obj.name[0] == 'A')
+            {
+                
+            }
+      /*  {
+            if (!obj.GetComponent<AudioSource>())
+            {
+                obj.AddComponent<AudioSource>().clip = Resources.Load<AudioClip>("audio/диктофон");
+            }
+            obj.GetComponent<AudioSource>().Play();
+        }
+        else if (obj.GetComponent<Image>().sprite == Resources.Load<Sprite>("icons/записка"))
+        {
+            NoteChecked.SetActive(!NoteChecked.activeInHierarchy);
+        }
+            */
+        else
+        {
+
+        }
+        
+    }
+}
