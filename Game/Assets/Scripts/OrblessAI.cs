59b84bbb38f68feba23978f88bd1b8eb88449e82
MrHungerrr <egor06060600@gmail.com>
2019-03-10 22:37:19 +0300

    123

--- Game/Assets/Scripts/OrblessAI.cs
+++ Game/Assets/Scripts/OrblessAI.cs
@@ -18,9 +18,5 @@ public class OrblessAI : MonoBehaviour
       dist3 = transform.position - player.transform.position;
       dist = dist3.magnitude;
       player.GetComponent<Sonar>().Distance(dist);
-<<<<<<< HEAD
-=======
-      //Debug.Log(dist);
->>>>>>> 4b909f14589da36a47d3d68290691112a7a852fb
     }
 }
